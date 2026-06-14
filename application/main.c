959f07bc78798d1a085ea44eb386dda6053fc026
ialbaraai <colnarxy@gmail.com>
2026-06-14 18:28:07 +0400

    feat: added necessary libs: `nuklear`, `cglm`, `tinyfd`, `luajit`

--- application/main.c
+++ application/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <core.h>
 
 int main(void)
 {
-	printf("To be done!\n");
+	string_t str = {0};
+	core_string_init(&str, 64);
+
+	printf("To be done soon, what is your name? ");
+	core_string_input(&str);
+
+	printf("Oh nice name %s! nice to meet you!\n", str.data);
+
+	core_string_destroy(&str);
 
 	return 0;
 }
